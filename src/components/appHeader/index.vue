2a7fb849f3fe8313be841af053617692bdb58962
yh.zhang <yh.zhang@cyber-insight.com>
2022-06-05 20:47:46 +0800

    ✨ home nav buy

--- src/components/appHeader/index.vue
+++ src/components/appHeader/index.vue
@@ -71,7 +71,7 @@
                     </svg>
                   </div>
                   <div class="sub-nav">
-                    <a href="https://apeswap.finance/jungle-farms" target="_blank" style="display: flex;align-items: center">
+                    <a href="https://apeswap.finance/swap?inputCurrency=0xe9e7cea3dedca5984780bafc599bd69add087d56&outputCurrency=0x89c1af791d7b4cf046dca8fa10a41dd2298a6a3f" target="_blank" style="display: flex;align-items: center">
                       Buy
                     </a>
                     <a href="https://derify.finance/stake" target="_blank">
