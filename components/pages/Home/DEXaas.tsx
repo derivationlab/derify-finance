82714dc571c576c0e37e4173e213b4ae1984bd44
derivationstaff001 <hans@derify.org>
2022-08-24 02:27:33 +0800

    base dom is over

--- components/pages/Home/DEXaas.tsx
+++ components/pages/Home/DEXaas.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react'
 import useTranslation from 'next-translate/useTranslation'
+import Marquee from "react-fast-marquee"
 
 import Button from '@@/common/Button'
 import Image from '@@/common/Image'
@@ -11,23 +12,23 @@ const DEXaas: FC = () => {
   return (
     <>
       <section className="web-home-dexaas">
-        <h2>{t('DEXaas.Title')}</h2>
-        <p>{t('DEXaas.Info')}</p>
-        <aside>
-          <Button dark to="https://docs.derify.finance/whitepaper/community-and-governance/broker">
-            {t('DEXaas.LearnMore')}
-          </Button>
-          <Button to="https://derify.exchange/broker">{t('DEXaas.ApplyBroker')}</Button>
-        </aside>
+        <main>
+          <h2 dangerouslySetInnerHTML={{__html: t('DEXaas.Title')}} />
+          <p>{t('DEXaas.Info')}</p>
+          <aside>
+            <Button dark to="https://docs.derify.finance/whitepaper/community-and-governance/broker">
+              {t('DEXaas.LearnMore')}
+            </Button>
+            <Button to="https://derify.exchange/broker">{t('DEXaas.ApplyBroker')}</Button>
+          </aside>
+        </main>
       </section>
       <section className="web-home-dexaas-logos">
-        <ul>
-          {list.map((_, index) => (
-            <li key={index}>
-              <Image src={`website/partner/${index + 1}.png`} alt="" />
-            </li>
-          ))}
-        </ul>
+          <Marquee gradient={false} pauseOnHover>
+            {list.map((_, index) => (
+              <Image key={index} src={`website/partner/${index + 1}.png`} alt="" />
+            ))}
+          </Marquee>
       </section>
     </>
   )
