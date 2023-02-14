968ad672936f37e14a75df51aa00ad485681d59c
derivationstaff001 <hans@derify.org>
2023-02-14 16:53:35 +0800

    opt code

--- components/common/Header/index.tsx
+++ components/common/Header/index.tsx
@@ -43,23 +43,23 @@ const Header: FC = () => {
               <span onClick={() => changeActiveFunc(1)}>{t('Nav.Community')}</span>
               <ul>
                 <li>
-                  <Image src="website/icon/twitter.svg" />
+                  <Image alt="" src="website/icon/twitter.svg" />
                   <ALink to="https://twitter.com/DerifyProtocol">Twitter</ALink>
                 </li>
                 <li>
-                  <Image src="website/icon/telegram.svg" />
+                  <Image alt="" src="website/icon/telegram.svg" />
                   <ALink to="https://t.me/DerifyProtocol_Official">Telegram</ALink>
                 </li>
                 <li>
-                  <Image src="website/icon/discord.svg" />
+                  <Image alt="" src="website/icon/discord.svg" />
                   <ALink to="https://discord.gg/kSR6tz2pdm">Discord</ALink>
                 </li>
                 <li>
-                  <Image src="website/icon/medium.svg" />
+                  <Image alt="" src="website/icon/medium.svg" />
                   <ALink to="https://derify.medium.com/">Medium</ALink>
                 </li>
                 <li>
-                  <Image src="website/icon/github.svg" />
+                  <Image alt="" src="website/icon/github.svg" />
                   <ALink to="https://github.com/derivationlab">Github</ALink>
                 </li>
               </ul>
@@ -71,7 +71,7 @@ const Header: FC = () => {
               <span onClick={() => changeActiveFunc(2)}>{t('Nav.DRFToken')}</span>
               <ul>
                 <li>
-                  <ALink to="https://apeswap.finance/swap?inputCurrency=0xe9e7cea3dedca5984780bafc599bd69add087d56&outputCurrency=0x89c1af791d7b4cf046dca8fa10a41dd2298a6a3f">
+                  <ALink to="https://pancakeswap.finance/swap?inputCurrency=0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56&outputCurrency=0x89C1Af791d7B4cf046Dca8Fa10a41Dd2298A6a3F">
                     {t('Nav.Buy')}
                   </ALink>
                 </li>
